65103c00a53f4129f43f6217f5f52065f39a1514
Jennifer H Thompson <writejthompson@gmail.com>
2025-03-10 14:41:07 -0600

    it is working - successfully switched to grpahql and apollo from REST

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import "./App.css"; 
+import { Routes, Route } from "react-router-dom"; // Import Routes here
 import Navbar from './components/Navbar';
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
 
-// Set up the GraphQL endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql', // Our GraphQL API endpoint
+  uri: 'http://localhost:4000/graphql', 
 });
 
-// Attach token for authentication
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
@@ -21,29 +20,44 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-// Initialize Apollo Client and wrap apollo around the rountes
 const client = new ApolloClient({
-  link: authLink.concat(httpLink), // Attach auth middleware
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Navbar />
+      <div className="app-container">
+        <Navbar />
+        <div className="content-container">
           <Routes>
             <Route path="/" element={<SearchBooks />} />
             <Route path="/saved" element={<SavedBooks />} />
             <Route path="*" element={<h1 className="display-2">Wrong page!</h1>} />
           </Routes>
         </div>
-      </Router>
+      </div>
     </ApolloProvider>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"; // Ensure Bootstrap is loaded first
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import SearchBooks from "./pages/SearchBooks";
-import SavedBooks from "./pages/SavedBooks";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <App>
-        <Routes>
-          <Route path="/" element={<SearchBooks />} />
-          <Route path="/saved" element={<SavedBooks />} />
-          <Route path="*" element={<h1 className="display-2">Wrong page!</h1>} />
-        </Routes>
-      </App>
+      <App />
     </Router>
   </React.StrictMode>
 );
+
